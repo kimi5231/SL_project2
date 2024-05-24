8ed6dfac14fecdfcec4dd109a09ca13d4f31e5d1
kimi5231 <djwjod@naver.com>
2024-05-25 01:31:04 +0900

    메타 선택 시 해당 메타에서 얻을 수 있는 정보 리스트를 출력하도록 명령어 구현
    +정규식을 이용해 명령어 대신 숫자를 이용해서도 요청할 수 있도록 구현
    +메타와 메타 이름 명령어 처리를 다른 함수로 분리

--- source/malangie.py
+++ source/malangie.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 class Malangie:
     def __init__(self):
         self.running = True
-        self.last_commend = 'commend'
-        self.p = re.compile(r'[1-9]')
+        self.last_commend = ''
+        self.p = re.compile(r'\d')
         self.url = 'https://api.telegram.org/bot6766400298:AAHttzF1j9Jjx5dk5g8ptT2guJsCxQy2F70'
         self.page_url = 'https://lolchess.gg/meta'
         self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
@@ -47,23 +47,47 @@ class Malangie:
         if text == '명령어':
             self.last_commend = 'commend'
             send_text = """
-메타: 현 시즌 추천 메타 목록
-종료: 프로그램 종료
+1. 메타: 현 시즌 추천 메타 목록을 보여줍니다.
+2. 종료: 프로그램을 종료합니다.
             """
             self.send_message(chat_id, send_text)
         elif text == '메타':
-            self.last_commend = 'meta'
-            send_text = ''
-            i = 1
-            for t in self.meta_and_link.keys():
-                send_text += f'{i}. {t}\n'
-                i += 1
-            send_text += '\n\n정보를 알고 싶은 메타의 이름을 입력해주세요.'
-            self.send_message(chat_id, send_text)
+            self.process_meta_commend(chat_id)
+        elif text in self.meta_and_link.keys():
+            self.process_meta_name_commend(chat_id)
         elif self.p.search(text):
-            pass
+            if self.last_commend == 'commend':
+                if text == '1':
+                    self.process_meta_commend(chat_id)
+                elif text == '2':
+                    self.running = False
+            elif self.last_commend == 'meta':
+                if 0 < int(text) < len(self.meta) + 1:
+                    self.process_meta_name_commend(chat_id)
         elif text == '종료':
             self.running = False
+
+    def process_meta_commend(self, chat_id):
+        self.last_commend = 'meta'
+        send_text = ''
+        i = 1
+        for t in self.meta_and_link.keys():
+            send_text += f'{i}. {t}\n'
+            i += 1
+        send_text += '\n\n정보를 알고 싶은 메타의 이름을 입력해주세요.'
+        self.send_message(chat_id, send_text)
+
+    def process_meta_name_commend(self, chat_id):
+        self.last_commend = 'meta name'
+        send_text = """
+1. 배치표: 추천 메타의 레벨별 배치표를 보여줍니다.
+2. 증강체: 추천 메타에 맞는 증강체를 등급별로 보여줍니다.
+3. 렙업: 스테이지별 렙업 추천을 보여줍니다. 
+4. 챔피언: 추천 메타에 포함되는 챔피언의 목록을 보여줍니다.
+5. 주요 아이템: 추천 메타의 주요 아이템과 아이템을 끼는 챔피언의 목룍을 보여줍니다.
+6. 챔피언 이름: 해당 챔피언이 아이템을 끼는 챔피언일 경우 주요 아이템 목룍을 보여줍니다.
+                                """
+        self.send_message(chat_id, send_text)
 
     def send_message(self, chat_id, text):
         r = requests.get(f'{self.url}/sendMessage', params={'chat_id': chat_id, 'text': text})
